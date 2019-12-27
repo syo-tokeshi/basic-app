8f256de881064e1a367420a6480c10cfbb914871
[user_name] <[user_email_address]>
2019-12-27 19:34:13 +0900

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,17 +16,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
     @post = Post.find_by(id:params[:id])
   end
+
   def update
     post = Post.find(params[:id])
     post.update(post_params)
     redirect_to posts_path
   end
-  
-  
+
+  def destroy
+    post = Post.find(params[:id])
+    post.delete
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title,:content)
