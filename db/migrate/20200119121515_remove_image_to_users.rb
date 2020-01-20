f781ef2447956d2a00e2caf99be2476742c79840
[user_name] <[user_email_address]>
2020-01-20 13:01:20 +0900

    g

--- db/migrate/20200119121515_remove_image_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageToUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :image
-  end
-end
--- db/migrate/20200119122842_change_posts_columns.rb
+++
@@ -1,5 +0,0 @@
-class ChangePostsColumns < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :image, :string
-  end
-end
