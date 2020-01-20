49b398e81b60510909a33c6d3f2b661952811c6a
[user_name] <[user_email_address]>
2020-01-20 12:35:46 +0900

    g

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments,dependent: :destroy
   validates  :title,:content, {presence:true}
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
   # has_one_attached :image
 end
