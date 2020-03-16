e30d472567a6d1f30f7e47c43db97acfddbe89c0
[user_name] <[user_email_address]>
2020-03-16 17:12:42 +0900

    s

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 process resize_to_limit: [400, 400]
 
 if Rails.env.production?
