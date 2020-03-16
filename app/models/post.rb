454912bab12fe1800fc3468f559aba06e6851888
[user_name] <[user_email_address]>
2020-03-16 16:14:23 +0900

    s

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-
+  mount_uploader :image, ImageUploader
   has_many :comments,dependent: :destroy #ポストはたくさんのコメントとを持つ
   # belongs_to :user
   validates  :title,:content, {presence:true}
