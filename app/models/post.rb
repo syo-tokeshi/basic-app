53579f989c2869095a3ed04400f74f2eefab64be
[user_name] <[user_email_address]>
2020-03-24 15:35:25 +0900

    change

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   has_many :comments,dependent: :destroy #ポストはたくさんのコメントとを持つ
   validates  :title,:content, {presence:true}
 
+#投稿詳細画面でユーザー情報を表示するためのメソッド
+  def user
+    return User.find_by(id: self.user_id)
+  end
+
 end
